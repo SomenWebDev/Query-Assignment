56d4c22ba7c0b6fd719bd6d31395ad033c6c55e5
Somen Singha <somensingha18@gmail.com>
2026-07-10 01:28:04 +0600

    added

--- query.sql
+++ query.sql
@@ -25,7 +25,44 @@ CREATE TABLE matches (
     tournament_category VARCHAR(50) NOT NULL,
     base_ticket_price NUMERIC(10,2) NOT NULL,
     match_status VARCHAR(20) NOT NULL CHECK (
-        match_status IN ('Available', 'Selling Fast', 'Sold Out')
+        match_status IN ('Available', 'Selling Fast', 'Sold Out','Postponed')
     )
 );
+-- insert data into matches table
+
+INSERT INTO matches (match_id, fixture, tournament_category, base_ticket_price, match_status) VALUES
+(101, 'Real Madrid vs Barcelona', 'Champions League', 150.00, 'Available'),
+(102, 'Man City vs Liverpool', 'Premier League', 120.00, 'Selling Fast'),
+(103, 'Bayern Munich vs PSG', 'Champions League', 130.00, 'Available'),
+(104, 'AC Milan vs Inter Milan', 'Serie A', 90.00, 'Sold Out'),
+(105, 'Juventus vs Roma', 'Serie A', 80.00, 'Available');
+
+
+-- create booking table
+CREATE TABLE bookings (
+    booking_id INT PRIMARY KEY,
+ 
+    user_id INT NOT NULL
+        CONSTRAINT fk_booking_user
+        REFERENCES users(user_id),
+ 
+    match_id INT NOT NULL
+        CONSTRAINT fk_booking_match
+        REFERENCES matches(match_id),
+ 
+    seat_number VARCHAR(10),
+ 
+    payment_status VARCHAR(20)
+        CHECK (payment_status IN ('Confirmed', 'Pending', 'Cancelled', 'Refunded')),
+ 
+    total_cost NUMERIC(10,2) NOT NULL
+);
+-- insert into bookings table
+INSERT INTO bookings (booking_id, user_id, match_id, seat_number, payment_status, total_cost) VALUES
+(501, 1, 101, 'A-12', 'Confirmed', 150.00),
+(502, 1, 102, 'B-04', 'Confirmed', 120.00),
+(503, 2, 101, 'A-13', 'Confirmed', 150.00),
+(504, 2, 101, NULL, NULL, 150.00),
+(505, 3, 102, 'C-20', 'Pending', 120.00);
+ 
 
